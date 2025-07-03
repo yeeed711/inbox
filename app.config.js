42b1a174e081d52c7649d71c68b7a226cf2ecef4
yeeed <yeeed.dev@gmail.com>
2025-07-03 11:25:26 +0900

    chore: 환경 설정 파일(app.config.js) 추가 및 .gitignore 업데이트, EAS 빌드 설정 변경, 패키지 버전 업데이트

--- app.config.js
+++ app.config.js
@@ -1,21 +1,21 @@
-// app.config.cjs
 module.exports = ({ config }) => {
+  const dev = process.env.APP_ENV === "development";
   // EAS 빌드에서 전달된 환경 변수 가져오기
-  const appEnv = process.env.APP_ENV || "development";
+  // const appEnv = process.env.APP_ENV || "development";
 
   // 환경별 앱 이름 설정
-  const appName = appEnv === "production" ? "Inbox" : `Inbox (${appEnv})`;
+  // const appName = appEnv === "production" ? "Inbox" : `Inbox (${appEnv})`;
 
   // 환경별 아이콘 경로
-  const iconBasePath = "./assets/images";
-  const iconPath = `${iconBasePath}/icon-${appEnv}.png`;
-  const adaptiveIconPath = `${iconBasePath}/adaptive-icon-${appEnv}.png`;
-  const splashIconPath = `${iconBasePath}/splash-icon-${appEnv}.png`;
+  // const iconBasePath = "./assets/images";
+  // const iconPath = `${iconBasePath}/icon-${appEnv}.png`;
+  // const adaptiveIconPath = `${iconBasePath}/adaptive-icon-${appEnv}.png`;
+  // const splashIconPath = `${iconBasePath}/splash-icon-${appEnv}.png`;
 
   // 환경별 번들 ID (선택 사항)
-  const baseBundleId = "com.anonymous.inbox";
-  const bundleId =
-    appEnv === "production" ? baseBundleId : `${baseBundleId}.${appEnv}`;
+  // const baseBundleId = "com.anonymous.inbox";
+  // const bundleId =
+  // appEnv === "production" ? baseBundleId : `${baseBundleId}.${appEnv}`;
 
   return {
     expo: {
@@ -24,27 +24,37 @@ module.exports = ({ config }) => {
           projectId: "3c8cdebe-7d49-4a4b-a314-7c3e2f2a3564",
         },
       },
-      name: appName,
+      name: "inbox",
       slug: "inbox",
       version: "1.0.0",
       orientation: "portrait",
-      icon: "./assets/images/icon.png",
+      icon: dev
+        ? "./assets/icons/ios-light-dev.png"
+        : "./assets/icons/ios-icon.png",
       scheme: "inbox",
       userInterfaceStyle: "automatic",
       newArchEnabled: true,
       ios: {
         supportsTablet: true,
-        bundleIdentifier: bundleId,
+        bundleIdentifier: "com.yeeed.inbox",
         icon: {
-          dark: "./assets/images/icon-dark.png",
+          dark: dev
+            ? "./assets/icons/ios-dark.png"
+            : "./assets/icons/ios-dark.png",
+          light: dev
+            ? "./assets/icons/ios-light-dev.png"
+            : "./assets/icons/ios-light.png",
+          tinted: dev
+            ? "./assets/icons/ios-tinted.png"
+            : "./assets/icons/ios-tinted.png",
         },
       },
       android: {
         adaptiveIcon: {
-          foregroundImage:
-            appEnv === "production"
-              ? "./assets/images/adaptive-icon.png"
-              : adaptiveIconPath,
+          foregroundImage: "./assets/icons/adaptive-icon.png",
+          // appEnv === "production"
+          //   ? "./assets/images/adaptive-icon.png"
+          //   : adaptiveIconPath,
           backgroundColor: "#ffffff",
         },
         edgeToEdgeEnabled: true,
@@ -63,10 +73,10 @@ module.exports = ({ config }) => {
         [
           "expo-splash-screen",
           {
-            image:
-              appEnv === "production"
-                ? "./assets/images/splash-icon.png"
-                : splashIconPath,
+            image: "./assets/images/1024.png",
+            // appEnv === "production"
+            //   ? "./assets/images/splash-icon.png"
+            //   : splashIconPath,
             imageWidth: 200,
             resizeMode: "contain",
             backgroundColor: "#ffffff",
